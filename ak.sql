4de5cb7b66031f0853f92c5ee2c55759655b0f1f
arjun98k <arjunkandekar14@gmail.com>
2025-03-18 21:24:20 +0530

    add first sql query

--- ak.sql
+++ ak.sql
@@ -1,3 +1,13 @@
-CREATE DATABASE arj;
-drop database arj;
+
 create database aki;
+use aki;
+
+create table aksh (
+id int primary key,
+name varchar(40),
+age int not null
+);
+
+insert into aksh values(1,"Arjun",23);
+insert into aksh values(2,"lotus",22);
+select * from aksh;
