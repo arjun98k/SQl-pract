887e448f9599d6f37793aa5faf3de8663734080c
arjun98k <arjunkandekar14@gmail.com>
2025-03-20 00:03:26 +0530

    add first sql query

--- ak.sql
+++ ak.sql
@@ -12,5 +12,5 @@ age int not null
 insert into aksh values(1,"Arjun",23);
 insert into aksh values(2,"lotus",22);
 insert into aksh values(3,"adi",24);
-select name from aksh;
+select name , age from aksh;
 show databases;
