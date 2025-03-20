c6ff49ff88cd8042e81168fef5fe34778cebc4ca
arjun98k <arjunkandekar14@gmail.com>
2025-03-20 10:44:55 +0530

    sql assgnment

--- sqlassignment.sql
+++ sqlassignment.sql
@@ -10,4 +10,9 @@ salary int
 insert into empinfo values(1,"arjun",25000);
 insert into empinfo values(2,"aditi",26000);
 insert into empinfo values(3,"amit",24000);
+insert into empinfo(id,name,salary)
+values
+(11,"akshraj",26000),
+(22,"takshraj",27000);
+
 select* from empinfo;
