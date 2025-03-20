4c24aabd1418e316f07fcc27b50a2d53e5d5d1ce
arjun98k <arjunkandekar14@gmail.com>
2025-03-20 10:41:01 +0530

    sql assgnment

---
+++ sqlassignment.sql
@@ -0,0 +1,13 @@
+create database employee;
+use employee;
+
+create table empinfo(
+id int primary key,
+name varchar(50),
+salary int
+);
+
+insert into empinfo values(1,"arjun",25000);
+insert into empinfo values(2,"aditi",26000);
+insert into empinfo values(3,"amit",24000);
+select* from empinfo;
