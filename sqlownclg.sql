dce19988242475e451c118aa35ab3df25109bc01
arjun98k <arjunkandekar14@gmail.com>
2025-03-20 20:37:27 +0530

    own db custom

---
+++ sqlownclg.sql
@@ -0,0 +1,21 @@
+create database college;
+
+use college;
+
+create table student(
+rollno int primary key,
+name varchar(50),
+marks int not null,
+grade varchar(1),
+city varchar(20)
+
+);
+
+insert into student(rollno, name , marks, grade ,city)
+values
+(1,"arjun",70,"B","pune"),
+(2,"amit",75,"B","mumbai"),
+(3,"akshraj",80,"A","pune"),
+(4,"sanika",85,"A","mumbai"),
+(5,"ankita",90,"A","delhi"),
+(6,"vijaya",99,"A","pune");
