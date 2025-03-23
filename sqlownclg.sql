d15fd86d8d25e719ded655e23804f254f34f2045
arjun98k <arjunkandekar14@gmail.com>
2025-03-23 21:47:43 +0530

    sql concept

--- sqlownclg.sql
+++ sqlownclg.sql
@@ -2,6 +2,19 @@ create database college;
 
 use college;
 
+create table dept(
+id int primary key,
+name varchar(50)
+
+);
+
+create table teacher(
+id int primary key,
+name varchar(50),
+dept_id int,
+foreign key  (dept_id) references dept(id)
+);
+
 create table student(
 rollno int primary key,
 name varchar(50),
@@ -10,6 +23,11 @@ grade varchar(1),
 city varchar(20)
 
 );
+
+
+
+
+
 
 insert into student(rollno, name , marks, grade ,city)
 values
